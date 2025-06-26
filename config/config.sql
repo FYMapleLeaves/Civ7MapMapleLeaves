f09c2d19b153ac180e0639035fe53ad590090ba3
FYMapleLeaves <1069533792@qq.com>
2025-06-26 23:08:44 +0800

    Fix bug with version 1.2.2
    Fix bug with version 1.2.2

--- config/config.sql
+++ config/config.sql
@@ -2,4 +2,4 @@
 INSERT INTO Maps 
     (File,                                          Name,                           Description,                                SortIndex)
 VALUES
-    ('{maple-leaves-map}maps/ml_inland_sea.js',     'LOC_MAP_ML_INLAND_SEA_NAME',   'LOC_MAP_MAP_ML_INLAND_SEA_DESCRIPTION',    210);
+    ('{maple-leaves-map}maps/ml_inland_sea.js',     'LOC_MAP_ML_INLAND_SEA_NAME',   'LOC_MAP_ML_INLAND_SEA_DESCRIPTION',    210);
--- maps/ml_inland_sea.js
+++ maps/ml_inland_sea.js
@@ -11,7 +11,7 @@ import * as globals from '/base-standard/maps/map-globals.js';
 import * as utilities from '/base-standard/maps/map-utilities.js';
 import { addNaturalWonders } from '/base-standard/maps/natural-wonder-generator.js';
 import { generateResources } from '/base-standard/maps/resource-generator.js';
-import { addVolcanoes } from '/base-standard/maps/volcano-generator.js';
+import { addTundraVolcanoes, addVolcanoes } from '/base-standard/maps/volcano-generator.js';
 import { assignAdvancedStartRegions } from '/base-standard/maps/assign-advanced-start-region.js';
 import { generateDiscoveries } from '/base-standard/maps/discovery-generator.js';
 import { generateSnow, dumpPermanentSnow } from '/base-standard/maps/snow-generator.js';
@@ -29,9 +29,6 @@ function requestMapData(initParams) {
 function generateMap() {
     console.log("Generating a map!");
     console.log(`Age - ${GameInfo.Ages.lookup(Game.age).AgeType}`);
-
-    console.log("Maple_leaves' Map Optimizations!");
-
     let iWidth = GameplayMap.getGridWidth();
     let iHeight = GameplayMap.getGridHeight();
     let uiMapSize = GameplayMap.getMapSize();
@@ -39,6 +36,10 @@ function generateMap() {
     let mapInfo = GameInfo.Maps.lookup(uiMapSize);
     if (mapInfo == null)
         return;
+    let iNumNaturalWonders = mapInfo.NumNaturalWonders;
+    let iTilesPerLake = mapInfo.LakeGenerationFrequency;
+    let iNumPlayers1 = mapInfo.PlayersLandmass1;
+    let iNumPlayers2 = mapInfo.PlayersLandmass2;
     // Establish continent boundaries
     let iOceanWaterColumns = globals.g_OceanWaterColumns;
     let westContinent = {
@@ -55,23 +56,54 @@ function generateMap() {
         north: iHeight - globals.g_PolarWaterRows,
         continent: 0
     };
-    let startSectors;
-    let iNumPlayers1 = mapInfo.PlayersLandmass1;
-    let iNumPlayers2 = mapInfo.PlayersLandmass2;
-    let iNumNaturalWonders = mapInfo.NumNaturalWonders;
-    let iTilesPerLake = mapInfo.LakeGenerationFrequency;
-    let iStartSectorRows = mapInfo.StartSectorRows;
-    let iStartSectorCols = mapInfo.StartSectorCols;
-    let iRandom = TerrainBuilder.getRandomNumber(2, "East or West");
-    if (iRandom == 1) {
-        let iNum1 = iNumPlayers1;
-        let iNum2 = iNumPlayers2;
-        iNumPlayers1 = iNum2;
-        iNumPlayers2 = iNum1;
+    let startSectors = [];
+    let iStartSectorRows = 0;
+    let iStartSectorCols = 0;
+    let startPosition = Configuration.getMapValue("StartPosition");
+    if (startPosition == null) {
+        startPosition = Database.makeHash('START_POSITION_STANDARD');
     }
-    let bHumanNearEquator = utilities.needHumanNearEquator();
-    startSectors = chooseStartSectors(iNumPlayers1, iNumPlayers2, iStartSectorRows, iStartSectorCols, bHumanNearEquator);
-    createLandmasses(iWidth, iHeight, westContinent, eastContinent, iStartSectorRows, iStartSectorCols, startSectors);
+    startPosition = Number(BigInt.asIntN(32, BigInt(startPosition))); // Convert to signed int32.
+    let startPositionHash = Database.makeHash("START_POSITION_BALANCED");
+    let bIsBalanced = (startPosition == startPositionHash);
+    if (bIsBalanced) {
+        console.log("Balanced Map");
+        let iRandom = TerrainBuilder.getRandomNumber(2, "East or West");
+        if (iRandom == 1) {
+            let iNum1 = iNumPlayers1;
+            let iNum2 = iNumPlayers2;
+            iNumPlayers1 = iNum2;
+            iNumPlayers2 = iNum1;
+        }
+        let bHumanNearEquator = utilities.needHumanNearEquator();
+        iStartSectorRows = mapInfo.StartSectorRows;
+        iStartSectorCols = mapInfo.StartSectorCols;
+        startSectors = chooseStartSectors(iNumPlayers1, iNumPlayers2, iStartSectorRows, iStartSectorCols, bHumanNearEquator);
+        dumpStartSectors(startSectors);
+        createLandmasses(iWidth, iHeight, westContinent, eastContinent, iStartSectorRows, iStartSectorCols, startSectors);
+        utilities.applyCoastalErosionAdjustingForStartSectors(westContinent, eastContinent, .02, 1.5, .8, iStartSectorRows, iStartSectorCols, startSectors);
+        utilities.applyCoastalErosionAdjustingForStartSectors(eastContinent, eastContinent, .02, 1.5, .8, iStartSectorRows, iStartSectorCols, startSectors);
+        utilities.addPlotTags(iHeight, iWidth, eastContinent.west);
+    }
+    else {
+        console.log("Standard Map");
+        let iRandom = TerrainBuilder.getRandomNumber(2, "East or West");
+        if (iRandom == 1) {
+            let iNum1 = iNumPlayers1;
+            let iNum2 = iNumPlayers2;
+            iNumPlayers1 = iNum2;
+            iNumPlayers2 = iNum1;
+        }
+        let bHumanNearEquator = utilities.needHumanNearEquator();
+        iStartSectorRows = mapInfo.StartSectorRows;
+        iStartSectorCols = mapInfo.StartSectorCols;
+        startSectors = chooseStartSectors(iNumPlayers1, iNumPlayers2, iStartSectorRows, iStartSectorCols, bHumanNearEquator);
+        dumpStartSectors(startSectors);
+        createLandmasses(iWidth, iHeight, westContinent, eastContinent, iStartSectorRows, iStartSectorCols, startSectors);
+        utilities.applyCoastalErosionAdjustingForStartSectors(westContinent, eastContinent, .02, 1.5, .8, iStartSectorRows, iStartSectorCols, startSectors);
+        utilities.applyCoastalErosionAdjustingForStartSectors(eastContinent, eastContinent, .02, 1.5, .8, iStartSectorRows, iStartSectorCols, startSectors);
+        utilities.addPlotTags(iHeight, iWidth, eastContinent.west);
+    }
     TerrainBuilder.validateAndFixTerrain();
     expandCoasts(iWidth, iHeight);
     utilities.adjustOceanPlotTags(iNumPlayers1 > iNumPlayers2);
@@ -80,6 +112,8 @@ function generateMap() {
     addMountains(iWidth, iHeight);
     addVolcanoes(iWidth, iHeight);
     generateLakes(iWidth, iHeight, iTilesPerLake);
+    utilities.adjustLakePlotTags(westContinent, true);
+    utilities.adjustLakePlotTags(eastContinent, false);
     AreaBuilder.recalculateAreas();
     TerrainBuilder.buildElevation();
     addHills(iWidth, iHeight);
@@ -88,6 +122,7 @@ function generateMap() {
     TerrainBuilder.validateAndFixTerrain();
     TerrainBuilder.defineNamedRivers();
     designateBiomes(iWidth, iHeight);
+    addTundraVolcanoes(iWidth, iHeight);
     addNaturalWonders(iWidth, iHeight, iNumNaturalWonders);
     TerrainBuilder.addFloodplains(4, 10);
     addFeatures(iWidth, iHeight);
@@ -95,7 +130,6 @@ function generateMap() {
     AreaBuilder.recalculateAreas();
     TerrainBuilder.storeWaterData();
     generateSnow(iWidth, iHeight);
-    dumpStartSectors(startSectors);
     dumpContinents(iWidth, iHeight);
     dumpTerrain(iWidth, iHeight);
     dumpElevation(iWidth, iHeight);
@@ -103,7 +137,7 @@ function generateMap() {
     dumpBiomes(iWidth, iHeight);
     dumpFeatures(iWidth, iHeight);
     dumpPermanentSnow(iWidth, iHeight);
-    generateResources(iWidth, iHeight, westContinent, eastContinent, iNumPlayers1, iNumPlayers2);
+    generateResources(iWidth, iHeight);
     startPositions = assignStartPositions(iNumPlayers1, iNumPlayers2, westContinent, eastContinent, iStartSectorRows, iStartSectorCols, startSectors);
     generateDiscoveries(iWidth, iHeight, startPositions);
     dumpResources(iWidth, iHeight);
@@ -268,8 +302,6 @@ function createLandmasses(iWidth, iHeight, continent1, continent2, iStartSectorR
             let terrain = globals.g_FlatTerrain;
             let iRandom = TerrainBuilder.getRandomNumber(iBuffer, "Random Top/Bottom Edges");
             let iRandom2 = TerrainBuilder.getRandomNumber(iBuffer2, "Random Left/Right Edges");
-            // Initialize plot tag
-            TerrainBuilder.setPlotTag(iX, iY, PlotTags.PLOT_TAG_NONE);
             //  Must be water if at the poles
             if (iY < continent1.south + iRandom || iY >= continent1.north - iRandom) {
                 terrain = globals.g_OceanTerrain;
@@ -408,13 +440,6 @@ function createLandmasses(iWidth, iHeight, continent1, continent2, iStartSectorR
                 }
             }        
 
-            // Add plot tag if applicable
-            if (terrain != globals.g_OceanTerrain && terrain != globals.g_CoastTerrain) {
-                utilities.addLandmassPlotTags(iX, iY, continent2.west);
-            }
-            else {
-                utilities.addWaterPlotTags(iX, iY, continent2.west);
-            }
             TerrainBuilder.setTerrainType(iX, iY, terrain);
         }
     }
